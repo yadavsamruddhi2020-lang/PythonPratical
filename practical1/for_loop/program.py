ba9442935da1cb0b6fb9aa0ef150aa45736d5554
Samruddhi <yadavsamruddhi2020@gamil.com>
2026-07-24 21:49:28 +0530

    for loop ques

--- practical1/for_loop/program.py
+++ practical1/for_loop/program.py
@@ -6,8 +6,30 @@
 
 # print even upto n
 
-n=int(input("enter num"))
-for i in range(1,n+1):
-    if i%2==0:
-        print(i)
+# n=int(input("enter num"))
+# for i in range(0,n+1):
+#     if i%2==0:
+#         print(i)
         
+
+# # print odd numbers
+# n=int(input("enter num"))
+# for i in range(0,n+1):
+#     if i%2!=0:
+#         print(i)
+
+
+# print 4 16 36 64 100
+
+# n=int(input("enter num:"))
+# for i in range (1,n+1):
+#     if i%2==0:
+#         print(i*i)
+
+
+# print 1 2 4 8 16 32 -----n^2
+
+# n=int(input("enter num:"))
+# for i in range (n):
+#     print(2**i," ")
+
---
+++ practical1/for_loop/tempCodeRunnerFile.py
@@ -0,0 +1,5 @@
+# print odd numbers
+# n=int(input("enter num"))
+# for i in range(0,n+1):
+#     if i%2!=0:
+#         print(i
