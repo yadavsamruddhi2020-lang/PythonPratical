fbeeb787b894c1f43ffbe193beec1aeba84566a0
Samruddhi <yadavsamruddhi2020@gamil.com>
2026-07-28 20:04:47 +0530

    for loop more que

--- practical1/for_loop/program.py
+++ practical1/for_loop/program.py
@@ -33,3 +33,55 @@
 # for i in range (n):
 #     print(2**i," ")
 
+# A B C
+# A B C
+# A B C
+# for i in range(3):
+#     for j in range(3):
+#         print(chr(65+j),end=" ")
+#     print()
+
+
+# A
+# A B
+# A B C
+# A B C D
+# A B C D E
+
+# n=int(input("enter the number:"))
+# for i in range(1,n+1):
+#     for j in range(i):
+#         print(chr(65+j),end=" ")
+#     print()
+
+
+# A B C D
+# A B C
+# A B
+# A
+# n=int(input("enter n:"))
+# for i in range(n,0,-1):
+#     for j in range(i):
+#         print(chr(65+j),end=" ")
+#     print()
+
+# 1
+# 1 2
+# 1 2 3
+# n =int(input("enter n:"))
+# for i in range(1,n+1):
+#     for j in range(1,i+1):
+#         print(j,end=" ")
+#     print()
+
+# 1 2 3
+# 1 2
+# 1
+n=int(input("enter n:"))
+for i in range (1,n+1):
+    for j in range (i):
+        print(i,end=" ")
+    print()
+
+
+
